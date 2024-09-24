ba7639b6a66175ba4051b87b30a1bc100ace3857
chongyongrui <88470129+chongyongrui@users.noreply.github.com>
2024-09-24 21:03:14 +0800

    fix db dump

--- server/Dbdump/caddyai_gamestats.sql
+++ server/Dbdump/caddyai_gamestats.sql
@@ -33,7 +33,7 @@ CREATE TABLE `gamestats` (
   `scores` text NOT NULL,
   `course` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `gamestats` (
 
 LOCK TABLES `gamestats` WRITE;
 /*!40000 ALTER TABLE `gamestats` DISABLE KEYS */;
-INSERT INTO `gamestats` VALUES (1,'033c62cc-8dc6-46f5-befe-db5a86017932','2024-08-24 14:52:36',-1,'test@test.com',99,'Black','{\"1\":5,\"2\":6,\"3\":5,\"4\":6,\"5\":8,\"6\":6,\"7\":5,\"8\":4,\"9\":2,\"10\":4,\"11\":5,\"12\":6,\"13\":7,\"14\":6,\"15\":6,\"16\":6,\"17\":6,\"18\":6}','Seletar Country Club');
+INSERT INTO `gamestats` VALUES (1,'033c62cc-8dc6-46f5-befe-db5a86017932','2024-08-24 14:52:36',-1,'test@test.com',99,'Black','{\"1\":5,\"2\":6,\"3\":5,\"4\":6,\"5\":8,\"6\":6,\"7\":5,\"8\":4,\"9\":2,\"10\":4,\"11\":5,\"12\":6,\"13\":7,\"14\":6,\"15\":6,\"16\":6,\"17\":6,\"18\":6}','Seletar Country Club'),(2,'bdb12f0b-ccae-4714-8f79-df74afb1207f','2024-08-26 15:36:15',-1,'yrchong2000@gmail.com',91,'Blue','{\"1\":6,\"2\":5,\"3\":6,\"4\":7,\"5\":4,\"6\":3,\"7\":6,\"8\":5,\"9\":4,\"10\":6,\"11\":3,\"12\":5,\"13\":4,\"14\":6,\"15\":4,\"16\":6,\"17\":6,\"18\":5}','Seletar Country Club');
 /*!40000 ALTER TABLE `gamestats` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-25 21:45:58
+-- Dump completed on 2024-09-24 21:02:55
